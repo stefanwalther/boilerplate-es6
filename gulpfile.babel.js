8cba43597b05ffacd56e9d5e987227f2736e1e8a
Stefan Walther <Stefan.Walther@qlik.com>
2016-10-22 00:10:04 +0200

    build: Copy non JS files to the ./dist folder

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,6 +5,7 @@ import path from 'path';
 import del from 'del';
 
 const paths = {
+  es6Path: './src/**/*.*',
   es6: [ './src/**/*.js', './src/**/*.json' ],
   es5: './dist',
   // Must be absolute or relative to source map
@@ -17,7 +18,7 @@ gulp.task( 'clean:dist', () => {
   ] );
 } );
 
-gulp.task( 'build', [ 'clean:dist' ], () => {
+gulp.task( 'build', [ 'clean:dist', 'copy:nonJs' ], () => {
   return gulp.src( paths.es6 )
     .pipe( sourceMaps.init() )
     .pipe( babel( {
@@ -26,6 +27,15 @@ gulp.task( 'build', [ 'clean:dist' ], () => {
     .pipe( sourceMaps.write( '.', { sourceRoot: paths.sourceRoot } ) )
     .pipe( gulp.dest( paths.es5 ) );
 } );
+
+// Copy all the non JavaScript files to the ./dist folder
+gulp.task( 'copy:nonJs', () => {
+
+  return gulp.src( [ paths.es6Path, '!' + paths.es6 ] )
+    .pipe( gulp.dest( paths.es5 ) )
+
+} );
+
 gulp.task( 'watch', [ 'build' ], () => {
   gulp.watch( paths.es6, [ 'build' ] );
 } );
